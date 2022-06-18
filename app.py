fca3994b3d15b481003f68cc6c2cfe5a2fd99cf1
Maxim Zeyfert <maxim.zeyfert@gmail.com>
2022-06-18 22:36:13 +0300

    add: app template

---
+++ app.py
@@ -0,0 +1,14 @@
+#!/home/maxim/.env/bin/python
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'test'
+
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        port=8080,
+    )
